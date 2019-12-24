b3e980424fd4b3e763fe8bb90b0d51cb37bacbe7
Donald Wu <yeukfei02@gmail.com>
2019-12-24 12:19:41 +0800

    update-ui-#25: update contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import EmailIcon from '@material-ui/icons/Email';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,12 +13,20 @@ const useStyles = makeStyles(theme => ({
 function Contact() {
   const classes = useStyles();
 
+  const handleEmailIconClick = () => {
+    window.open('mailto:yeukfei02@gmail.com');
+  }
+
+  const handleGithubIconClick = () => {
+    window.open('https://github.com/yeukfei02');
+  }
+
   return (
     <div className="mt-5 d-flex justify-content-center">
       <Paper className={`${classes.root} mx-4 w-75 text-center`}>
         <h4>Please contact us in email or visit our github repo</h4>
-        <h5>Email: yeukfei02@gmail.com</h5>
-        <h5>Github: yeukfei02</h5>
+        <EmailIcon style={{ cursor: 'pointer' }} className="mr-3" fontSize="large" onClick={handleEmailIconClick} />
+        <GitHubIcon style={{ cursor: 'pointer' }} fontSize="large" onClick={handleGithubIconClick} />
       </Paper>
     </div >
   )
