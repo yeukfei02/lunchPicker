43b36ccb48b7685a9c0a2f81e04affcfe028b257
Donald Wu <yeukfei02@gmail.com>
2019-12-31 17:30:50 +0800

    add-donorbox-in-ui-#75: add donorbox in contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -22,12 +22,30 @@ function Contact() {
   }
 
   return (
-    <div className="mt-5 d-flex justify-content-center">
-      <Paper className={`${classes.root} mx-4 w-75 text-center`}>
-        <h4>contact us via email or visit our github repo</h4>
-        <EmailIcon style={{ cursor: 'pointer' }} className="mr-3" fontSize="large" onClick={handleEmailIconClick} />
-        <GitHubIcon style={{ cursor: 'pointer' }} fontSize="large" onClick={handleGithubIconClick} />
-      </Paper>
+    <div>
+      <div className="mt-5 d-flex justify-content-center">
+        <Paper className={`${classes.root} mx-4 w-75 text-center`}>
+          <h4>contact us via email or visit our github repo</h4>
+          <EmailIcon style={{ cursor: 'pointer' }} className="mr-3" fontSize="large" onClick={handleEmailIconClick} />
+          <GitHubIcon style={{ cursor: 'pointer' }} fontSize="large" onClick={handleGithubIconClick} />
+        </Paper>
+      </div>
+      <div className="my-5 d-flex justify-content-center">
+        <Paper className={`${classes.root} mx-4 w-75 text-center`}>
+          <iframe
+            title="donorbox"
+            allowpaymentrequest=""
+            frameBorder="0"
+            height="900px"
+            name="donorbox"
+            scrolling="no"
+            seamless="seamless"
+            src="https://donorbox.org/embed/donate-for-lunch-picker-better-features-and-development?default_interval=o"
+            style={{ maxWidth: '500px', minWidth: '310px', maxHeight: 'none !important', minHeight: window.innerHeight, padding: '1em 2.8em' }}
+            width="100%">
+          </iframe>
+        </Paper>
+      </div>
     </div>
   )
 }
