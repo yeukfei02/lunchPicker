c0add0741ff3d0307a5ca93dc715da7443fd2068
Donald Wu <yeukfei02@gmail.com>
2020-01-01 12:09:44 +0800

    fix-code: add tooltip and donorbox button in contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Tooltip from '@material-ui/core/Tooltip';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
@@ -26,24 +27,22 @@ function Contact() {
       <div className="mt-5 d-flex justify-content-center">
         <Paper className={`${classes.root} mx-4 w-75 text-center`}>
           <h4>contact us via email or visit our github repo</h4>
-          <EmailIcon style={{ cursor: 'pointer' }} className="mr-3" fontSize="large" onClick={handleEmailIconClick} />
-          <GitHubIcon style={{ cursor: 'pointer' }} fontSize="large" onClick={handleGithubIconClick} />
+          <Tooltip title="yeukfei02@gmail.com" placement="bottom">
+            <EmailIcon style={{ cursor: 'pointer' }} className="mr-3" fontSize="large" onClick={handleEmailIconClick} />
+          </Tooltip>
+          <Tooltip title="yeukfei02" placement="bottom">
+            <GitHubIcon style={{ cursor: 'pointer' }} fontSize="large" onClick={handleGithubIconClick} />
+          </Tooltip>
         </Paper>
       </div>
       <div className="my-5 d-flex justify-content-center">
         <Paper className={`${classes.root} mx-4 w-75 text-center`}>
-          <iframe
-            title="donorbox"
-            allowpaymentrequest=""
-            frameBorder="0"
-            height="900px"
-            name="donorbox"
-            scrolling="no"
-            seamless="seamless"
-            src="https://donorbox.org/embed/donate-for-lunch-picker-better-features-and-development?default_interval=o"
-            style={{ maxWidth: '500px', minWidth: '310px', maxHeight: 'none !important', minHeight: window.innerHeight, padding: '1em 2.8em' }}
-            width="100%">
-          </iframe>
+          <a
+            className="dbox-donation-button"
+            style={{ background: '#ff0000', color: '#fff', textDecoration: 'none', fontFamily: 'Verdana,sans-serif', display: 'inline-block', fontSize: '16px', padding: '13px 17px', borderRadius: '2px', boxShadow: '0 1px 0 0 #1f5a89', textShadow: '0 1px rgba(0, 0, 0, 0.3)' }}
+            href="https://donorbox.org/donate-for-lunch-picker-better-features-and-development?default_interval=o">
+            Donate
+          </a>
         </Paper>
       </div>
     </div>
