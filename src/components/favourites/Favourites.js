c888ce79026e45bcb8eef324243766e7377a6b37
Donald Wu <yeukfei02@gmail.com>
2020-01-08 00:35:15 +0800

    add-favourites-ui-#115: add loading in favourites ui

--- src/components/favourites/Favourites.js
+++ src/components/favourites/Favourites.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { red } from '@material-ui/core/colors';
 import _ from 'lodash';
 import axios from 'axios';
 
@@ -17,6 +20,10 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2),
     flexGrow: 1,
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: red[500],
+  },
 }));
 
 function Favourites() {
@@ -24,10 +31,18 @@ function Favourites() {
 
   const [favourites, setFavourites] = useState([]);
 
+  const [open, setOpen] = useState(true);
+
   const [openSuccessAlert, setOpenSuccessAlert] = useState(false);
   const [message, setMessage] = useState('');
 
   const currentToken = localStorage.getItem('firebaseCurrentToken');
+
+  useEffect(() => {
+    if (!_.isEmpty(currentToken)) {
+      getFavourites(currentToken);
+    }
+  }, [currentToken]);
 
   useEffect(() => {
     if (openSuccessAlert === true) {
@@ -39,10 +54,12 @@ function Favourites() {
   }, [openSuccessAlert, message]);
 
   useEffect(() => {
-    if (!_.isEmpty(currentToken)) {
-      getFavourites(currentToken);
+    if (!_.isEmpty(favourites)) {
+      setTimeout(() => {
+        setOpen(false)
+      }, 1500);
     }
-  }, [currentToken]);
+  }, [favourites]);
 
   const getFavourites = (currentToken) => {
     axios.get(
@@ -85,10 +102,18 @@ function Favourites() {
       );
     } else {
       renderDiv = (
-        <div className="mt-4 d-flex justify-content-center">
-          <Paper className={`${classes.root} mx-4 w-75 text-center`}>
-            <h4>There are no result.</h4>
-          </Paper>
+        <div>
+          <Backdrop
+            className={classes.backdrop}
+            open={open}
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
+          <div className="mt-4 d-flex justify-content-center">
+            <Paper className={`${classes.root} mx-4 w-75 text-center`}>
+              <h4>There are no result.</h4>
+            </Paper>
+          </div>
         </div>
       );
     }
